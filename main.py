648bd2d2edfea4d8d70e41435eb998abb020e775
Aleksey <lexa.shvetsoff@yandex.ru>
2022-03-20 17:52:46 +0800

    title and author of first book obtained

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from bs4 import BeautifulSoup
 
 
 def check_for_redirect(response):
@@ -7,22 +8,32 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-directory_name = 'books'
-if not os.path.exists(directory_name):
-    os.makedirs(directory_name, exist_ok=True)
+book_url = 'https://tululu.org/b1/'
+response = requests.get(book_url)
+response.raise_for_status()
 
-url = 'https://tululu.org/txt.php'
+soup = BeautifulSoup(response.text, 'lxml')
+book_title = soup.find('div', id='content').find('h1')
+text = book_title.text.split('::')
+title, author = text[0].strip(), text[1].strip()
+print(f'Название: {title}')
+print(f'Автор: {author}')
+# directory_name = 'books'
+# if not os.path.exists(directory_name):
+#     os.makedirs(directory_name, exist_ok=True)
 
-for i in range(1, 11):
-    payload = {"id": i}
+# url = 'https://tululu.org/txt.php'
 
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
+# for i in range(1, 11):
+#     payload = {"id": i}
 
-    try:
-        check_for_redirect(response)
-        filename = f'books/id{i}.txt'
-        with open(filename, 'wb') as file:
-            file.write(response.content)
-    except requests.HTTPError:
-        print('Такой книги нет!')
+#     response = requests.get(url, params=payload)
+#     response.raise_for_status()
+
+#     try:
+#         check_for_redirect(response)
+#         filename = f'books/id{i}.txt'
+#         with open(filename, 'wb') as file:
+#             file.write(response.content)
+#     except requests.HTTPError:
+#         print('Такой книги нет!')

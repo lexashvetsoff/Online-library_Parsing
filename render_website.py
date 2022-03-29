584ebbb4d5ebb731328c009281573223b7644ce8
Aleksey <lexa.shvetsoff@yandex.ru>
2022-03-29 12:07:35 +0800

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,15 +1,9 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+# from http.server import HTTPServer, SimpleHTTPRequestHandler
+from livereload import Server
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 import os
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
 
 with open('data_books.json', 'r', encoding="utf8") as file:
     data_books_json = file.read()
@@ -20,12 +14,27 @@ if os.sep == '\\':
         book['img_src'] = book['img_src'].replace('\\', '/')
         book['book_path'] = book['book_path'].replace('\\', '/')
 
-rendered_page = template.render(
-    books = data_books
-)
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    template = env.get_template('template.html')
+    
+    rendered_page = template.render(
+        books = data_books
+    )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+on_reload()
+
+server = Server()
+
+server.watch('template.html', on_reload)
+server.serve(root='.')
+
+# server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+# server.serve_forever()
